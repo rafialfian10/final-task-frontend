87666c126972760b304cd9ec12086f3ef7c1528d
rafialfian10 <rafialfian770@gmail.com>
2023-03-06 20:26:27 +0700

    frontend

--- src/components/description_download/DescriptionDownload.jsx
+++ src/components/description_download/DescriptionDownload.jsx
@@ -15,8 +15,8 @@ const DescriptionDownload = () => {
                     <br />
                     <p className='detail-desc-info'>Dalam pelayaran menyusuri Venesia, Heidy bertemu raksasa bermata biru. Graeme MacLeod, pria yang mencuri napasnya di pertemuan pertama mereka. Meski ketertarikan di antara mereka begitu besar, Heidy tidak berniat menjalin asmara singkat. Graeme harus dilupakan. Ketika apa yang terjadi di Venesia tidak bisa tetap ditinggal di Venesia, Heidy mulai goyah. Apalagi Graeme ternyata lelaki gigih yang mengejarnya hingga ke Jakarta dan tak putus asa tatkala ditolak. Meski akhirnya satu per satu rahasia kelam lelaki itu terbuka, Heidy justru kian jatuh cinta. Pertanyaannya, apakah cinta memang benar-benar mampu menyatukan mereka?</p>
 
-                    <div className='content-btn-download'>
-                        <Button type='submit' className='btn-download'>Download</Button>
+                    <div className='btn-download'>
+                        <Button type='submit'>Download</Button>
                     </div>
                 </div>
             </div>
